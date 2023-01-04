3c1effae2ca9b5e4bb8e9f24fe92bbc082e0c60a
cloy123 <cosoygnoy@gmail.com>
2023-01-04 18:59:24 +0300

    (6) добавил внедрение зависимостей

---
+++ MerchandiseShop.Persistence/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using MerchandiseShop.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchandiseShop.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["dbConnection"];
+            services.AddDbContext<MerchShopDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IMerchShopDbContext>(provider => provider.GetService<MerchShopDbContext>());
+            return services;
+        }
+    }
+}
